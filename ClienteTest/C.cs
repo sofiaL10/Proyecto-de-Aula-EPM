95556f05254f6379460eed098a5cd2b670a75b65
sofiaL10 <140664493+sofiaL10@users.noreply.github.com>
2024-03-22 18:34:25 -0500

    Update C.cs

--- ClienteTest/C.cs
+++ ClienteTest/C.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ProyectoAulaTest
-{
-    internal class C
-    {
-    }
-}
